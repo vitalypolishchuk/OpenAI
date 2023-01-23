68982286146f8ed771a869b310a0beb15c4162d7
vitalypolishchuk <vetal.polishuk@gmail.com>
2023-01-23 14:12:43 +0200

    hero section

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import videoBg from "./additional/video-hero.mp4";
 
 export default function App() {
   return (
-    <section className="openai-background">
+    <section className="openai-hero">
       <video src={videoBg} loop muted />
+      <div className="title">
+        <span>Try chatGPT &#8599;</span>
+        <span className="title-2">Introducing ChatGPT release</span>
+        <span>Try &#8599;</span>
+      </div>
       <button className="btn">ChatGPT</button>
+      <p>Hello world, how are you?</p>
     </section>
   );
 }
