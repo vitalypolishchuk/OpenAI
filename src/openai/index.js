f92c7769545cc59885ff55efaf5e98f8c57d8e79
vitalypolishchuk <vetal.polishuk@gmail.com>
2023-01-23 21:50:48 +0200

    hero-section

--- src/openai/index.js
+++ src/openai/index.js
@@ -1,13 +1,25 @@
 import "./sass/hero.scss";
 import videoBg from "../additional/video-hero.mp4";
+import thumbnail from "../additional/thumbnail.jpg";
+import { useRef, useEffect } from "react";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 export default function MainPage() {
+  const imgRef = useRef(null);
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    videoRef.current.onloadeddata = () => {
+      imgRef.current.classList.add("animation-opacity");
+    };
+  }, []);
+
   return (
     <Fragment>
       <section className="openai-hero">
-        <video src={videoBg} loop muted />
+        <video src={videoBg} ref={videoRef} loop muted />
+        <img src={thumbnail} ref={imgRef}></img>
         <div className="openai__title">
           <Link to="/chatgpt">Try ChatGPT ↗</Link>
           <Link to="/chatgpt" className="openai__title--spans">
