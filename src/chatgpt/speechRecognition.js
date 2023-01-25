5e6626986470fe6c60e655132bbf69976cdd767c
vitalypolishchuk <vetal.polishuk@gmail.com>
2023-01-25 20:02:44 +0200

    gpt voice-recognition

--- src/chatgpt/speechRecognition.js
+++ src/chatgpt/speechRecognition.js
@@ -1,99 +1,155 @@
 import "./sass/main.scss";
-import { useState, useEffect, useRef, React } from "react";
+import { useState, useEffect, useRef } from "react";
 import { v4 as uniqueId } from "uuid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import { faMicrophone, faBars, faPlus, faXmark } from "@fortawesome/free-solid-svg-icons";
 import barsSvg from "../additional/bars.svg";
-import { languages } from "./languages";
 
 export default function SpeechRecognition() {
-  const languageRef = useRef();
+  const [mainContainerHeight, setMainContainerHeight] = useState(100);
+  const [isListening, setIsListening] = useState(false);
+  const [text, setText] = useState("");
+  const [containerHeight, setContainerHeight] = useState(39);
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
   const barsRef = useRef();
-  const microphoneRef = useRef();
-  const resultRef = useRef();
-  const interimRef = useRef();
-
-  const [showInterim, setShowInterim] = useState(true);
-  const [interimText, setInterimText] = useState("");
-  let [resultText, setResultText] = useState("");
-  const [isSpeechOn, setIsSpeechOn] = useState(false);
-  const [language, setLanguage] = useState("");
-  const [selectedLanguage, setSelectedLanguage] = useState("");
-  const [recongition, setRecognition] = useState(null);
-  const [recording, setRecording] = useState(false);
+  const refMicrophone = useRef();
+  const refTextArea = useRef();
+  const refSideMenu = useRef();
+  const refOverlay = useRef();
 
   useEffect(() => {
-    if (recongition) {
-      generateSpeech();
-    }
-  }, [recongition]);
+    setMainContainerHeight(window.innerHeight - containerHeight - 40);
+  }, [containerHeight, innerHeight]);
 
-  function generateSpeech() {
-    // if the recording button was clicked
-    recongition.lang = languageRef.current.value; // setting up language
-    recongition.interimResults = true; // setting up interim results (not final)
-    barsRef.current.classList.remove("none"); // show recording svg
-    setIsSpeechOn(true); // let react now that we are recording
-    recongition.start(); // start recording
-    // recieve result. result can be either final or interim
-    let myresult;
-    recongition.onresult = (e) => {
-      const speechResult = e.results[0][0].transcript;
-      console.log(speechResult, e.results[0].isFinal);
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
 
-      // if result is final, show it in the resultText
-      if (e.results[0].isFinal) {
-        setResultText((resultText += " " + speechResult));
-        setShowInterim(false); // remove interim element
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleText();
+  }, [text]);
+
+  function handleListen() {
+    try {
+      const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognition = new SpeechRecognitionAPI();
+
+      recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.lang = "en-US";
+      if (isListening) {
+        refMicrophone.current.classList.add("microphone-active");
+
+        recognition.start();
+        barsRef.current.classList.remove("none");
+        recognition.onend = () => {
+          recognition.start();
+        };
       } else {
-        setInterimText(" " + speechResult);
+        recognition.stop();
+        refMicrophone.current.classList.remove("microphone-active");
+        recognition.onend = () => {};
+        barsRef.current.classList.add("none");
       }
-    };
-    recongition.onspeechend = () => {
-      // on speech end again call the function to continuously listen
-      setRecognition(new (window.speechRecognition || window.webkitSpeechRecognition)());
-    };
-
-    recongition.onerror = (e) => {
-      alert("Error Occured: ", e.error);
-    };
+      // recognition.onstart = () => {
+      //   console.log("Recognition is on");
+      // };
+      recognition.onresult = (e) => {
+        const transcript = Array.from(e.results)
+          .map((result) => result[0].transcript)
+          .join("");
+        setText(transcript);
+        recognition.onerror = (event) => {
+          throw new Error(event);
+        };
+      };
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  function generateLanguates() {
-    return languages.map((lang) => {
-      return (
-        <option key={uniqueId()} value={lang.code}>
-          {lang.name}
-        </option>
-      );
-    });
+  function onResize() {
+    handleText();
+    handleHeight();
   }
 
-  function speechToText() {
-    if (isSpeechOn) return stopSpeechToText(); // if speech recognition API is on, stop it
-    return setRecognition(new (window.speechRecognition || window.webkitSpeechRecognition)()); // if speech recognition API is off, launch it
+  function handleText() {
+    setText(refTextArea.current.value);
+
+    // set the height of the textarea to auto in order to find scrollHeight of the element
+    refTextArea.current.style.height = "auto";
+
+    // If scrollHeight < 200, we increase/decrease the height of the textarea depending on the scrollHeight
+    if (refTextArea.current.scrollHeight < window.innerHeight * 0.2) {
+      // use setTextAreaHeight to set the height of the container
+      setContainerHeight(refTextArea.current.scrollHeight + 21);
+      refTextArea.current.style.height = refTextArea.current.scrollHeight + "px";
+      refTextArea.current.style.overflowY = "hidden";
+    } else {
+      // if scrollHeight >= 200, we set height to 200 and add scroll
+      setContainerHeight(window.innerHeight * 0.2 + 21);
+      refTextArea.current.style.height = window.innerHeight * 0.2 + "px";
+      refTextArea.current.style.overflowY = "scroll";
+    }
   }
 
-  function stopSpeechToText() {
-    // stop speech to text functionality
-    recongition.stop();
-    barsRef.current.classList.add("none");
-    setIsSpeechOn(false);
+  function handleHeight() {
+    setInnerHeight(window.innerHeight);
+  }
+
+  function handleSideMenu() {
+    refSideMenu.current.style.left = "0";
+    refOverlay.current.classList.remove("hidden");
+    refOverlay.current.classList.add("visible-overlay");
+  }
+
+  function handleCloseSideMenu() {
+    refSideMenu.current.style.left = "-100%";
+    refOverlay.current.classList.add("hidden");
+    refOverlay.current.classList.remove("visible-overlay");
   }
 
   return (
-    <div className="speech-recongition">
-      <select name="language" id="language" ref={languageRef} value={language} onChange={(e) => setLanguage(e.target.value)}>
-        {generateLanguates()}
-      </select>
-      <span className="speech-recongition__icons">
+    <div className="gpt-main-container" style={{ height: mainContainerHeight + "px" }}>
+      <header className="gpt-header">
+        <header className="gpt-header__center">
+          <button onClick={handleSideMenu} className="gpt-header__menu gpt-btn">
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          <h5 className="gpt-header__text">useEffect and window.innerHeight</h5>
+          <button className="gpt-header__plus gpt-btn">
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </header>
+      </header>
+      <aside className="gpt-sidemenu" ref={refSideMenu}>
+        menu
+        <button onClick={handleCloseSideMenu} className="gpt-sidemenu__close gpt-btn">
+          <FontAwesomeIcon icon={faXmark} />
+        </button>
+      </aside>
+      <div className="chat-gpt" style={{ height: 60 + containerHeight + "px" }}>
         <img src={barsSvg} ref={barsRef} className="bars none" />
-        <FontAwesomeIcon icon={faMicrophone} ref={microphoneRef} className="microphone" onClick={speechToText} />
-      </span>
-      <div className="speech-recongition__result" ref={resultRef} spellCheck="false">
-        {showInterim && <p className="speech-recongition__interim">{interimText}</p>}
-        <div>{resultText}</div>
+        <div className="chat-gpt__result" style={{ height: containerHeight + "px" }}>
+          <textarea ref={refTextArea} rows={1} value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+        <FontAwesomeIcon icon={faMicrophone} ref={refMicrophone} className="microphone" onClick={() => setIsListening(!isListening)} />
+        <div className="chat-gpt__version">
+          <span>ChatGPT Jan 9 Version.</span>
+          <span>
+            Free Research Preview. Our goal is to make AI systems more natural and safe to interact with. Your feedback will help us improve.
+          </span>
+        </div>
       </div>
+      <div className="overlay hidden" ref={refOverlay}></div>
     </div>
   );
 }
