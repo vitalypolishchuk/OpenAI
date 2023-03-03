19d48cfc3ba7dd9f82d7bf3fd15442c6ee10b714
vitalypolishchuk <vetal.polishuk@gmail.com>
2023-03-03 19:53:26 +0200

    new chat-gpt engine

--- src/chatgpt/api.js
+++ src/chatgpt/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Configuration, OpenAIApi } from "openai";
 
-const url = "/completions";
+const url = "/chat/completions";
 
 const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
@@ -18,7 +18,7 @@ export function abortRequest(controller) {
 
 const data = {
   model: "",
-  prompt: "",
+  messages: [],
   temperature: 0.5,
   max_tokens: 1024,
 };
@@ -28,14 +28,16 @@ const headers = {
   Authorization: `Bearer ${API_KEY}`,
 };
 
-export async function apiCall(message = "Say this is a test", model = "text-davinci-003", signal) {
-  data.prompt = message;
+export async function apiCall(messages = "Say this is a test", model = "gpt-3.5-turbo", signal) {
+  data.messages = messages;
   data.model = model;
+
+  console.log(data);
 
   try {
     const response = await openai.axios.post(openai.basePath + url, data, { headers: headers, signal });
     console.log(response.data.choices);
-    return { message: response.data.choices[0].text };
+    return { message: response.data.choices[0].message.content };
   } catch (err) {
     throw err;
   }
